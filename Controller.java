05145ac098d2e5c93dae3e6f614fc4a332a04567
Kevin Phelan <81191184+phekev@users.noreply.github.com>
2022-04-13 22:58:52 +0100

    Updated controller & sort
    Moved methods from controller to sort.
    Renamed methods
    Added comments

--- Controller.java
+++ Controller.java
@@ -14,9 +14,12 @@ public class Controller {
 	
 	// Input size n for each test array
 	static final int[] BENCHMARK_ARRAYSIZE = {10, 100, 1000, 2500, 5000, 7500,100000};
+	
+	public enum dataType { RANDOM, PARTIAL }; 									// Types of data to be sorted.... random, partially sorted
 				
 
 	public static void main(String[] args) {
+				
 		
 		/*
 		 * Create an instance of each sorting algorithm
@@ -36,8 +39,8 @@ public class Controller {
 		int numOfTimesToRunTest = 10;
 		
 		Controller controller = new Controller();
-		controller.runRandomSortingTest(resultsOfBenchmark, numOfTimesToRunTest);
-		controller.runPartialSortingTest(resultsOfBenchmark, numOfTimesToRunTest);
+		controller.runTestWithRandomData(resultsOfBenchmark, numOfTimesToRunTest);
+		controller.runTest(resultsOfBenchmark, numOfTimesToRunTest);
 		
 		//Print results of benchmarks to the console
 		ResultsPrinter.print(resultsOfBenchmark, BENCHMARK_ARRAYSIZE);
@@ -50,22 +53,18 @@ public class Controller {
 	
 
 	//Runs the benchmarking test on randomly sorted data
-	private void runRandomSortingTest (List<Sort> sortingAlgorithms, int numOfTimesToRunTest) {
+	private void runTestWithRandomData (List<Sort> sortingAlgorithms, int numOfTimesToRunTest) {
 
 		// Run a test for each array of size n on each algorithm X number of times
 		for (int n : BENCHMARK_ARRAYSIZE) {
-
-			for(int j=0; j<sortingAlgorithms.size(); j++) {
-				double totalTestTime = 0.0; 
-				double averageTestTime = 0.0;
-				
-				totalTestTime += runOnRandomlySortedData(sortingAlgorithms.get(j), n, numOfTimesToRunTest);
-				averageTestTime = totalTestTime / numOfTimesToRunTest;
-				
-				sortingAlgorithms.get(j).randomSortBenchmarkResults.put(n , averageTestTime);
-			}
+			Sort.timeManyTests(sortingAlgorithms, numOfTimesToRunTest, n);
 		}	
 	}
+
+
+
+	
+	
 	
 	
 	//Runs the benchmarking test on partially sorted / nearly sorted arrays
@@ -74,37 +73,23 @@ public class Controller {
 		// Run a test for each array of size n on each algorithm X number of times
 		for (int n : BENCHMARK_ARRAYSIZE) {
 			
-			for(int j=0; j<sortingAlgorithms.size(); j++) {
+			for(int sortAlgoToTest=0; sortAlgoToTest<sortingAlgorithms.size(); sortAlgoToTest++) {
 				
 				double totalTestTime = 0.0; 
 				double averageTestTime = 0.0;
 				
-				totalTestTime += runOnPartiallySortedData(sortingAlgorithms.get(j), n, numOfTimesToRunTest);
+				totalTestTime += sortingAlgorithms.get(sortAlgoToTest)
+							.runOnPartiallySortedData(n, numOfTimesToRunTest);
 				averageTestTime = totalTestTime / numOfTimesToRunTest;
 				
-				sortingAlgorithms.get(j).partialSortBenchmarkResults.put(n , averageTestTime);
+				sortingAlgorithms.get(sortAlgoToTest).partialSortBenchmarkResults.put(n , averageTestTime);
 				
 			}
 		}
 	}
 	
 	
-	private double runOnRandomlySortedData(Sort sortAlgo, int n, int numOfIterations) {
-		double testTime = 0.0;
-		for (int i = 0; i<numOfIterations; i++) {
-			testTime += sortAlgo.timer(sortAlgo, CreateArrays.createRandomArray(n));
-		}
-		return testTime;
-	}
-	
-	
-	private double runOnPartiallySortedData(Sort sortAlgo, int n, int numOfIterations) {
-		double testTime = 0.0;
-		for (int i = 0; i<numOfIterations; i++) {
-			testTime += sortAlgo.timer(sortAlgo, CreateArrays.partialSortedArray(n));
-		}
-		return testTime;
-	}
+
 	
 	
 
--- Sort.java
+++ Sort.java
@@ -1,20 +1,56 @@
 package ctaProject.ComputationalThinkingWithAlgorithms;
 
 import java.util.LinkedHashMap;
+import java.util.List;
 
 public abstract class Sort {
 	
 	LinkedHashMap<Integer, Double> randomSortBenchmarkResults = new LinkedHashMap<Integer, Double>();		//Store results from tests ran on the pseudorandom number arrays
 	LinkedHashMap<Integer, Double> partialSortBenchmarkResults = new LinkedHashMap<Integer, Double>();		//Store results from tests ran on the partially sorted (or nearly ordered) arrays
 
-	// Sort#timer Times the running of the sorting algorithm
-	public double timer(Sort algo, int[] array) {
+	// Times the length to sort a single array for a given sorting algorithm
+	public double timeASingleTest(int[] array) {
 		long startTime = System.nanoTime();							
 		this.sort(array);								//Run the sorting algorithm on the array
 		long endTime = System.nanoTime();
 		long timeElapsed = endTime - startTime;
 		double elapsedMillis = (double) timeElapsed / 1000000;
 		return (Double) elapsedMillis;					//Wrapping in Double for storage in linkedHashMap
+	}
+	
+	// Times the total cumulative time to run the sorting algorithm for a given array size n
+	public static void timeManyTests(List<Sort> sortingAlgorithms, int numOfTimesToRunTest, int n) {
+		for(int sortAlgoToTest=0; sortAlgoToTest<sortingAlgorithms.size(); sortAlgoToTest++) {
+			double totalTestTime = 0.0; 
+			double averageTestTime = 0.0;
+			
+			totalTestTime += sortingAlgorithms.get(sortAlgoToTest)
+								.runOnRandomlySortedData(n, numOfTimesToRunTest);
+			averageTestTime = totalTestTime / numOfTimesToRunTest;
+			
+			sortingAlgorithms.get(sortAlgoToTest).randomSortBenchmarkResults.put(n , averageTestTime);
+		}
+	}
+	
+	// runOnRandomlySortedData runs a sorting algorithm on pseudorandom data
+	
+	// @param numOfIterations How many times the sorting algorithm  runs for array size n 
+	// @param n 	Size of the array
+	public double runOnRandomlySortedData(int n, int numOfIterations) {
+		double testTime = 0.0;
+		for (int i = 0; i<numOfIterations; i++) {
+			testTime += this.timeASingleTest(CreateArrays.createRandomArray(n));
+		}
+		return testTime;
+	}
+	
+	
+	public double runOnPartiallySortedData(int n, int numOfIterations) {
+		double testTime = 0.0;
+		for (int i = 0; i<numOfIterations; i++) {
+			testTime += this.timeASingleTest(CreateArrays.partialSortedArray(n));
+		}
+		return testTime;
 	}
 	
 	//Sort#sort to be implemented by each individual sorting class
